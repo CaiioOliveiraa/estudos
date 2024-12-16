698d22c6773f9a15ec3a3828a4517f312aea70e2
Caio Oliveira <oliveiracaio2@hotmail.com>
2024-12-16 16:53:57 -0300

    adiciona print sobrenome

--- variaveis/exercicio_variaveis.py
+++ variaveis/exercicio_variaveis.py
@@ -8,6 +8,7 @@ maior_de_idade = idade >= 18
 altura_metros = 1.85
 
 print('Nome:', nome)
+print('Sobrenome', sobrenome)
 print('Idade:', idade)
 print('Ano de Nascimento:', ano_nascimento)
 print('É maior de idade?', maior_de_idade)
